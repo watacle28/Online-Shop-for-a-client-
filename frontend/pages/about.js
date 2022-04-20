8b5dd5b60dcd219c06a410bcf12be829232896d4
cwangayi <cleopas.wangayi@amovos.com>
2022-04-20 03:54:06 +0200

    minor ui updates

---
+++ frontend/pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Lynux Espadrilles - About</title>
+      </Head>
+<p>
+    About us
+</p>
+    </div>
+  )
+}
+
+
+export default AboutPage
--- frontend/pages/products/[slug].js
+++ frontend/pages/products/[slug].js
@@ -12,52 +12,77 @@ const ProductPage = ({ product }) => {
   }
 
   return (
-    <div className="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mt-8">
+    <div className="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 mt-8">
       <Head>
-        <title>{product.title} product</title>
+        <title>{product.title}</title>
       </Head>
-      <div className="rounded-t-lg pt-2 pb-2 m-auto h-40 w-40">
-        <NextImage media={product.image} />
-      </div>
-      <div className="w-full p-5 flex flex-col justify-between">
-        <div>
-          <h4 className="mt-1 font-semibold text-lg leading-tight truncate text-gray-700">
-            {product.title} - ${product.price}
-          </h4>
-          <div className="mt-1 text-gray-600">{product.description}</div>
+      
+    <div class="flex items-center bg-white dark:bg-gray-800 rounded-lg shadow">
+        <div class="flex-auto w-1/2 h-full relative">
+            <NextImage media={product.image} />
         </div>
-
-        {product.status === "published" ? (
-          <button
-            className="snipcart-add-item mt-4 bg-white border border-gray-200 d hover:shadow-lg text-gray-700 font-semibold py-2 px-4 rounded shadow"
-            data-item-id={product.id}
-            data-item-price={product.price}
-            data-item-url={router.asPath}
-            data-item-description={product.description}
-            data-item-image={getStrapiMedia(
-              product.image.formats.thumbnail.url
-            )}
-            data-item-name={product.title}
-            v-bind="customFields"
-          >
-            Add to cart
-          </button>
-        ) : (
-          <div className="text-center mr-10 mb-1" v-else>
-            <div
-              className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-              role="alert"
-            >
-              <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                Coming soon...
-              </span>
-              <span className="font-semibold mr-2 text-left flex-auto">
-                This article is not available yet.
-              </span>
-            </div>
-          </div>
-        )}
-      </div>
+          <form class="flex-auto p-6">
+              <div class="flex flex-wrap">
+                  <h1 class="flex-auto text-xl font-semibold dark:text-gray-50">
+                      {product.title}
+                  </h1>
+                  <div class="text-xl font-semibold text-gray-500 dark:text-gray-300">
+                      R{product.price}
+                  </div>
+                  <div class="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2">
+                      {product.description}
+                  </div>
+                  <div class="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2">
+                      In stock
+                  </div>
+              </div>
+              <div class="flex items-baseline mt-4 mb-6 text-gray-700 dark:text-gray-300">
+              {product.sizes.length > 0 &&
+                        <div class="flex-auto text-lg font-semibold dark:text-gray-50 mr-auto">Sizes</div>}
+              <div class="space-x-2 flex justify-between">
+                      
+                          {product.sizes.map((size) => (
+                              <label class="text-center">
+                              <input type="radio" class="w-6 h-6 flex items-center justify-center bg-gray-100 dark:bg-gray-600 rounded-lg" name="size" value="xs"/>
+                                  {size.size}
+                              </label>
+                          ))}   
+              </div>
+                                  </div>
+                                  <div class="flex mb-4 text-sm font-medium">
+                                      {product.status === "published" ? (
+                <button
+                  className="snipcart-add-item py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
+                  data-item-id={product.id}
+                  data-item-price={product.price}
+                  data-item-url={router.asPath}
+                  data-item-description={product.description}
+                  data-item-image={getStrapiMedia(
+                    product.image.formats.thumbnail.url
+                  )}
+                  data-item-name={product.title}
+                  v-bind="customFields"
+                >
+                  Add to cart
+                </button>
+              ) : (
+                <div className="text-center mr-10 mb-1" v-else>
+                  <div
+                    className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+                    role="alert"
+                  >
+                    <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                      Coming soon...
+                    </span>
+                    <span className="font-semibold mr-2 text-left flex-auto">
+                      This article is not available yet.
+                    </span>
+                  </div>
+                </div>
+              )}
+                </div>
+          </form>
+        </div>
     </div>
   )
 }
